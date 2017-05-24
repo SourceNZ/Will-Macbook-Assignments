b1098b61ff95313ab488bccf0d9e17e2b988878f
William Idoine <hockey.will@gmail.com>
2017-05-24 20:59:10 +1200

    HTML 345 Assignment 3

---
+++ action_page.php
@@ -0,0 +1,45 @@
+        <div style="max-width: 800px;" class="w3-container w3-content w3-padding-16 ">
+
+    <img src="BioProfilePic.jpg" alt="Bio profile picture" height="300" width="220" >
+        </div>
+
+<form action="/action_page.php">
+
+    Which out of these exercises will be most beneficial? 
+    Read this then answer<br>
+    <input type="text" name="q1">
+    <br> 
+    What fitness/muscles are used for that sport. Read this then answer<br>
+    <input type="text" name="q2">    
+    <br>
+    How to  properly to maximize fitness for that sport? Read this then answer
+    <br> 
+    <input type="text" name="q3">
+    <br>
+    Ask them how it will improve their abilities. Read this then answer <br>
+    <input type="text" name="q4">
+    <input type="submit" value="Submit" onclick="location.href='4.html'">
+    </form>
+    <div align="right">
+
+    <input type="radio" name="person" value="Sarah" checked> Sarah<br>
+    <input type="radio" name="person" value="Nicole"> Nicole<br>
+    <input type="radio" name="person" value="Hannah"> Hannah <br>
+    <input type="radio" name="person" value="Anna"> Anna <br>
+    </div>
+    </div>
+
+
+      <div class="two-col">
+        <div class="col1">
+            <label for="field1">What fitness/muscles are used for that sport. Read this then answer</label>
+            <textarea rows="3" cols="50">
+            </textarea>
+        </div>
+
+        <div class="col2">
+        <input type="radio" name="person" value="Sarah" checked> Sarah<br>
+        <input type="radio" name="person" value="Nicole"> Nicole<br>
+        <input type="radio" name="person" value="Hannah"> Hannah <br>
+        <input type="radio" name="person" value="Anna"> Anna <br>
+    </div>
