984eeefd631aa8aa3457aebb7c2bd207b038974b
William Idoine <hockey.will@gmail.com>
2017-07-30 14:30:15 +1200

    Compsci 335 a1
    Added navigational hiding

--- Compsci 335 A1/ShowOrders.js
+++ Compsci 335 A1/ShowOrders.js
@@ -11,16 +11,16 @@ function getDestinations() {
     xhr.send(null);
 }
 function getId() {
-            var xhr = new XMLHttpRequest();
-            var uri = "http://redsox.uoa.auckland.ac.nz/BC/Open/Service.svc/booklist";
-            xhr.open("GET", uri, true);
-            xhr.setRequestHeader("Accept", "application/json");
-            xhr.onload = function () {
-                var version_d = document.getElementById("show_result");
-                //alert(xhr.responseText);
-                version_d.innerHTML = xhr.responseText;
-            }
-            xhr.send(null);
+    var xhr = new XMLHttpRequest();
+    var uri = "http://redsox.uoa.auckland.ac.nz/BC/Open/Service.svc/booklist";
+    xhr.open("GET", uri, true);
+    xhr.setRequestHeader("Accept", "application/json");
+    xhr.onload = function () {
+        var version_d = document.getElementById("show_result");
+        //alert(xhr.responseText);
+        version_d.innerHTML = xhr.responseText;
+    }
+    xhr.send(null);
 }
 
 function showDestinations(dest) {
@@ -35,5 +35,63 @@ function showDestinations(dest) {
         }
         tableContent += "<td>" + record.Id + "</td><td>" + record.Title + "</td></tr>\n";
     }
-    document.getElementById("showTab").innerHTML = tableContent;
+    document.getElementById("showTable").innerHTML = tableContent;
 }
+
+var currentTab = "";
+function showTabHome() {
+    if (currentTab != "TabHome") {
+        currentTab = "TabHome";
+        showNoTabs();
+        document.getElementById("SectionHome").style.display = "inline";
+
+    }
+
+
+}
+
+function showTabBlu() {
+    if (currentTab != "TabBlu") {
+        currentTab = "TabBlu";
+        showNoTabs();
+        document.getElementById("SectionBlu").style.display = "inline";
+    }
+}
+
+function showTabBooks() {
+    if (currentTab != "TabBooks") {
+        currentTab = "TabBooks";
+        showNoTabs();
+        document.getElementById("SectionBooks").style.display = "inline";
+    }
+}
+function showTabRegister() {
+    if (currentTab != "TabRegister") {
+        currentTab = "TabRegister";
+        showNoTabs();
+        document.getElementById("SectionRegister").style.display = "inline";
+    }
+}
+function showTabGuest() {
+    if (currentTab != "TabGuest") {
+        currentTab = "TabGuest";
+        showNoTabs();
+        document.getElementById("SectionGuest").style.display = "inline";
+    }
+}
+
+function showNoTabs() {
+
+    document.getElementById("SectionHome").style.display = "none";
+    document.getElementById("SectionBlu").style.display = "none";
+    document.getElementById("SectionBooks").style.display = "none";
+    document.getElementById("SectionRegister").style.display = "none";
+    document.getElementById("SectionGuest").style.display = "none";
+    
+    
+}
+window.onload = function () {
+            showTabHome();
+}
+
+
